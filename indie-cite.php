d17d2c6b5fcac14b4b02dfdaa84e15c8a91715e7
Matthias Pfefferle <pfefferle@gmail.com>
2020-02-24 12:06:55 +0100

    use textarea for cite/blockquote

--- indie-cite.php
+++ indie-cite.php
@@ -5,7 +5,7 @@
  * Description: Nice citation forms for the comments section
  * Author: Matthias Pfefferle
  * Author URI: https://notiz.blog
- * Version: 1.0.3
+ * Version: 1.0.4
  * License: MIT
  * License URI: https://opensource.org/licenses/MIT
  * Text Domain: indie-cite
@@ -34,12 +34,12 @@ function indie_cite_content() {
 	if ( get_the_title() ) { ?>
 	<p>
 		<label for="indie-cite-cite"><?php _e( 'HTML', 'indie-cite' ); ?></label>
-		<input id="indie-cite-cite" class="code" type="text" size="70" value="&lt;cite class=&quot;h-cite&quot;&gt;&lt;a class=&quot;u-url p-name&quot; href=&quot;<?php echo get_permalink(); ?>&quot;&gt;<?php echo get_the_title(); ?>&lt;/a&gt; (&lt;span class=&quot;p-author h-card&quot; title=&quot;<?php echo get_the_author(); ?>&quot;&gt;<?php echo get_the_author(); ?>&lt;/span&gt; &lt;time class=&quot;dt-published&quot; datetime=&quot;<?php echo get_the_date( 'c' ); ?>&quot;&gt;<?php echo get_the_date(); ?>&lt;/time&gt;)&lt;/cite&gt;">
+		<textarea id="indie-cite-cite" class="code" type="text" rows="5" cols="70">&lt;cite class=&quot;h-cite&quot;&gt;&lt;a class=&quot;u-url p-name&quot; href=&quot;<?php echo get_permalink(); ?>&quot;&gt;<?php echo get_the_title(); ?>&lt;/a&gt; (&lt;span class=&quot;p-author h-card&quot; title=&quot;<?php echo get_the_author(); ?>&quot;&gt;<?php echo get_the_author(); ?>&lt;/span&gt; &lt;time class=&quot;dt-published&quot; datetime=&quot;<?php echo get_the_date( 'c' ); ?>&quot;&gt;<?php echo get_the_date(); ?>&lt;/time&gt;)&lt;/cite&gt;</textarea>
 	</p>
 	<?php } else { ?>
 	<p>
 		<label for="indie-cite-blockquote"><?php _e('HTML', 'indie-cite'); ?></label>
-		<input id="indie-cite-blockquote" class="code" type="text" size="70" value="&lt;blockquote&gt;&lt;p&gt;<?php echo get_the_excerpt(); ?>&lt;/p&gt;&lt;small&gt;—&nbsp;by &lt;a href=&quot;<?php echo get_permalink(); ?>&quot; class=&quot;h-card&quot; title=&quot;<?php echo get_the_author(); ?>&quot;&gt;<?php echo get_the_author(); ?>&lt;/a&gt;&lt;/small&gt;&lt;/blockquote&gt;">
+		<textarea id="indie-cite-blockquote" class="code" type="text" rows="5" cols="70">&lt;blockquote&gt;&lt;p&gt;<?php echo get_the_excerpt(); ?>&lt;/p&gt;&lt;small&gt;—&nbsp;by &lt;a href=&quot;<?php echo get_permalink(); ?>&quot; class=&quot;h-card&quot; title=&quot;<?php echo get_the_author(); ?>&quot;&gt;<?php echo get_the_author(); ?>&lt;/a&gt;&lt;/small&gt;&lt;/blockquote&gt;</textarea>
 	</p>
 	<?php
 	}
