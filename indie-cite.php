283afaff23db2ff5ce43fc48e0436d45389288b9
Matthias Pfefferle <pfefferle@gmail.com>
2020-02-25 09:21:12 +0100

    show also if comments are closed

--- indie-cite.php
+++ indie-cite.php
@@ -31,17 +31,19 @@ function indie_cite_content() {
 	</p>
 	<?php
 	// some comment ;)
-	if ( get_the_title() ) { ?>
+	if ( get_the_title() ) {
+		?>
 	<p>
 		<label for="indie-cite-cite"><?php _e( 'HTML', 'indie-cite' ); ?></label>
 		<textarea id="indie-cite-cite" class="code" type="text" rows="5" cols="70">&lt;cite class=&quot;h-cite&quot;&gt;&lt;a class=&quot;u-url p-name&quot; href=&quot;<?php echo get_permalink(); ?>&quot;&gt;<?php echo get_the_title(); ?>&lt;/a&gt; (&lt;span class=&quot;p-author h-card&quot; title=&quot;<?php echo get_the_author(); ?>&quot;&gt;<?php echo get_the_author(); ?>&lt;/span&gt; &lt;time class=&quot;dt-published&quot; datetime=&quot;<?php echo get_the_date( 'c' ); ?>&quot;&gt;<?php echo get_the_date(); ?>&lt;/time&gt;)&lt;/cite&gt;</textarea>
 	</p>
 	<?php } else { ?>
 	<p>
-		<label for="indie-cite-blockquote"><?php _e('HTML', 'indie-cite'); ?></label>
+		<label for="indie-cite-blockquote"><?php _e( 'HTML', 'indie-cite' ); ?></label>
 		<textarea id="indie-cite-blockquote" class="code" type="text" rows="5" cols="70">&lt;blockquote&gt;&lt;p&gt;<?php echo get_the_excerpt(); ?>&lt;/p&gt;&lt;small&gt;—&nbsp;by &lt;a href=&quot;<?php echo get_permalink(); ?>&quot; class=&quot;h-card&quot; title=&quot;<?php echo get_the_author(); ?>&quot;&gt;<?php echo get_the_author(); ?>&lt;/a&gt;&lt;/small&gt;&lt;/blockquote&gt;</textarea>
 	</p>
-	<?php
+		<?php
 	}
 }
 add_action( 'comment_form_after', 'indie_cite_content', 99 );
+add_action( 'comment_form_comments_closed', 'indie_cite_content', 99 );
